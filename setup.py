5b07e05f28ce068be27657b3291ee5bce75bdf70
Pedro Rosa <pedro_sxbr@usp.br>
2024-01-23 00:54:42 +0100

    Created setup.py and Renamed files

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='GCE',
+    version='1.fun',
+    description='fork for using the GCE package',
+    author='Not Pedro',
+    author_email='jdoe@example.com',
+    packages=['GCE.data_loading'],
+    install_requires=[
+        'numpy',
+        'pillow',
+        'tifffile'
+    ],
+)
+
