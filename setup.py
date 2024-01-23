f9e109653b266ab9441f54a6883bd90f6e531b2f
Pedro <61952980+PSXBRosa@users.noreply.github.com>
2024-01-23 15:32:08 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='GCE',
-    version='1.fun',
+    version='1.0',
     description='fork for using the GCE package',
     author='Not Pedro',
     author_email='jdoe@example.com',
